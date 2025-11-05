4bcf3879f98d8e32ed0da0035a7bd59829a42296
purvesh <pujanmendhule@gmail.com>
2025-11-05 10:45:34 +0530

    secret key leaked problem solved and 1st commit for frontend

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,7 +1,7 @@
 const { GoogleGenAI } =require("@google/genai");
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({apiKey:"AIzaSyA8aQiu8me_x49K8IAEQcStBWha8qxRwUM"});
+const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
 
 
 
